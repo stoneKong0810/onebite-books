e4c603b8283457883c992429ac178a41c5988202
stoneKong0810 <stonekong2115@gmail.com>
2025-07-21 22:29:58 +0900

    앱 메타데이터 수정 및 서치바 레이아웃 추가, 인덱스 및 검색 페이지 구현

--- src/app/(with-searchBar)/search/page.tsx
+++ src/app/(with-searchBar)/search/page.tsx
@@ -4,5 +4,5 @@ export default async function Page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
-  return <div>검색 페이지 : {q}</div>;
+  return <>검색 페이지 : {q}</>;
 }
